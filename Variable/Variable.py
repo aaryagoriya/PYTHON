a1e44d7aea125e0bd806eae99b3351d6241a2d0c
Aarya Goriya <aaryagoriya@gmail.com>
2025-08-17 15:20:53 +1200

    Create Variable.py

---
+++ Variable/Variable.py
@@ -0,0 +1,58 @@
+# VARIABLES IN PYTHON
+"""A variable is like a container or a name tag that stores a value."""
+
+#Numeric datatypes
+#Integer 
+"""An integer is a whole number, it has no decimal point."""
+integer = 7
+
+#Float 
+"""A float is a number with a decimal point."""
+float = 07.00
+
+
+#Text datatypes
+#String
+"""A string is a piece of text, like letters, words, or sentences."""
+string = "Hello World"
+
+#Boolean datatypes
+"""Boolean is a type of value that can be only True or False."""
+boolean = True
+
+
+#Sequence datatypes
+#List 
+"""A list is a way to store many things together in one place."""
+list = [10, "hello", True, 3.14]
+
+#Tuple
+"""A tuple is like a list, but you cannot change it once you make it."""
+tuple = ("red", "green", "blue")
+
+#Range 
+"""A range is a way to create a list of numbers that go in order."""
+for i in range(10):
+    print(i)
+
+
+#Set
+"""set in Python is a collection of unique items with no specific order."""
+set = {1, 2, 3, 4}
+
+
+#Mapping datatypes
+#Dictionary 
+"""A dictionary is a collection of key-value pairs. Keys must be unique and immutable, and values can be any data type."""
+dict = {"name": "Aarya", "age": 30}
+
+
+#None datatype
+#None
+"""None is a special constant in Python that represents the absence of a value or a null value."""
+python = None
+print(type(python))
+
+"""Unconvered or Advance Python Datatypes{
+    complex, fronzenset, bytes, bytearray, memoryview
+}"""
