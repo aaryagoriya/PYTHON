3e3ecb461fb6c0b277e2ded2f79f6efe1de3728d
Aarya Goriya <aaryagoriya@gmail.com>
2025-09-03 19:52:09 +1200

    .

---
+++ Basics/Condition/if else.py
@@ -0,0 +1,9 @@
+a = int(input("Enter your age: "))
+
+if (a>18):
+    print("You are aboove the age of concent")
+    print("You are allowed to Go!")
+else:
+    print("You are below the age of concent")
+
+print("End of Program!")
