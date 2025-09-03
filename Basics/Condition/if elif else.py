4156f3b2768b7c89941712b491a00d2088ba274c
Aarya Goriya <aaryagoriya@gmail.com>
2025-09-03 19:56:16 +1200

    Update if elif else.py

---
+++ Basics/Condition/if elif else.py
@@ -0,0 +1,8 @@
+age = int(input("Enter the age: "))
+
+if(age > 18):
+    print("You are an adult.")
+elif(age < 0):
+    print("Your are entering an invaild age!")
+else:
+    print("Oops, you are not a adult.")
