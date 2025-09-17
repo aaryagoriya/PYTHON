f218102918ba340de9a63f688baed27f9cdd8230
Aarya Goriya <aaryagoriya@gmail.com>
2025-09-17 23:49:46 +1200

    Update Class.py

--- Object-Oriented Programming/Class.py
+++ Object-Oriented Programming/Class.py
@@ -1,5 +1,9 @@
 class python:
     def function1():
         x = int(input("Enter age: "))
-        if x > 18:
+        if(x > 18):
             print("Sorry, you are under age!")
+        elif(x<18):
+            print("Invalid input!")
+        else:
+            print("Welcome, you are above the permited age")
